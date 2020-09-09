4bd183a4c075582bf4fcc7fc0986a1d33d8688df
Kevin Wang <kcwang06@gmail.com>
2020-09-08 20:20:45 -0400

    hackernews nav set up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../css/navbar.css'
 
 export default function Navbar(props) {
 	return (
@@ -7,7 +8,7 @@ export default function Navbar(props) {
 			<div className='tabBox-main'>
 				<Link className='Logo' to='/'>
 					<h1 className='TabLogo' label='Logo'>
-						ProCollab
+						hackernews
 					</h1>
 				</Link>
 			</div>
@@ -21,19 +22,19 @@ export default function Navbar(props) {
 						</Link>
 					</div>
 					<div className='tabBox'>
-						<Link to='/Signup' state={'Signup'}>
-							<h3 className='TabLabel' label='Signup'>
-								Signup
+						<Link to='/Timeline' state={'Timeline'}>
+							<h3 className='TabLabel' label='Timeline'>
+								Timeline
 							</h3>
 						</Link>
 					</div>
-					<div className='tabBox'>
+					{/* <div className='tabBox'>
 						<Link to='/Profile' state={'Profile'}>
 							<h3 className='TabLabel' label='Profile'>
 								Profile
 							</h3>
 						</Link>
-					</div>
+					</div> */}
 				</div>
 			</div>
 		</div>
