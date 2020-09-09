30568b0728298837b1f0d42d4207f06fec5469d0
unknown <bbdavidbb@gmail.com>
2020-09-08 17:59:35 -0700

    revert?

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { MyProvider, AppContext } from './context'
 import Navbar from '../components/Navbar'
 import Home from '../components/Home'
+import Login from '../components/Login'
+import SignUp from '../components/Signup';
 
 export default class App extends React.Component {
+<<<<<<< HEAD
 	render() {
 		return (
 			<div className='App'>
@@ -30,3 +33,28 @@ export default class App extends React.Component {
 		)
 	}
 }
+=======
+  render()  {
+    return (
+      <div className = "App">
+        <BrowserRouter>
+          <MyProvider>
+            <AppContext.Consumer>
+          {(context) => {
+            return (
+              <React.Fragment>
+                <Navbar />
+                <Route path="/" exact component={ props => <Home {...props} {...context} />} />
+                <Route path="/Signup" exact component={ props => <SignUp {...props} {...context} />} />
+                <Route path="/Login" exact component={ props => <Login {...props} {...context} />} />
+              </React.Fragment>
+            );
+          }}
+          </AppContext.Consumer>
+          </MyProvider>
+         </BrowserRouter>
+      </div>
+    );
+  }
+}
+>>>>>>> davbranch
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app/App'
 import './index.css'
+import App from './app/App'
+
 
 ReactDOM.render(<App />, document.querySelector('#root'))
