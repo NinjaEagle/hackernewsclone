372844c0c6561d69246e516c199238428d360859
Samyak Kumar <samyakkumar@Samyaks-MacBook-Pro.local>
2020-09-10 17:00:00 -0400

    Add getPostList as a GET response

--- src/api/backend.js
+++ src/api/backend.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     mode: "cors",
-    baseURL: 'http://localhost:5001/hackernewsclone-bc4a1/us-central1/api',
+    baseURL: ' https://us-central1-hackernewsclone-bc4a1.cloudfunctions.net/api ',
     //baseURL: 'localhost:5001',
     // headers: {
     //     Authorization: ''
