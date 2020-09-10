c36db7e6efb10bc06030a0fe91126196e20b3c62
Jay Patel <jpatel10@toromail.csudh.edu>
2020-09-09 17:12:25 -0700

    added comment box

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Comment from './Comment'
 import { Container, Col, Row } from 'react-bootstrap'
 import './css/Home.scss'
 import Timeline from './Timeline'
@@ -14,7 +15,9 @@ export default class Home extends React.Component {
 
     render() {
         return (
+		
             <div className='blank'>
+					<Comment/>
 			<Container>
 				<Row>
 					<Col>
